a8eb7794972310dfd65ae32fef6f53a7bc57732d
jihoon0621 <42331481+jihoon0621@users.noreply.github.com>
2020-09-08 00:32:12 +0900

    #2.12 Recap
    #2.12 Recap

--- app.js
+++ app.js
@@ -3,19 +3,18 @@ import morgan from "morgan";
 import helmet from "helmet";
 import cookieParser from "cookie-parser";
 import bodyParser from "body-parser";
-import globalRouter from "./routers/globalRouter";
 import userRouter from "./routers/userRouter";
 import videoRouter from "./routers/videoRouter";
+import globalRouter from "./routers/globalRouter";
 import routes from "./routes";
 
 const app = express();
 
-app.use(cookieParser());
-app.use(bodyParser.json());
+app.use(cookieParser()); // 사용자 인증 시 필요한 미들웨어
+app.use(bodyParser.json()); // 사용자가 웹사이트로 전달하는 정보들을 검사하는 미들웨어
 app.use(bodyParser.urlencoded({ extended: true }));
-app.use(bodyParser());
-app.use(morgan("dev"));
-app.use(helmet());
+app.use(helmet()); // application 의 안정성을 위한 미들웨어
+app.use(morgan("dev")); // application 에서 발생하는 모든 일들을 logging 하는 미들웨어
 
 app.use(routes.home, globalRouter);
 app.use(routes.users, userRouter);
