5981a865b2a20af4fc08e6be7eabb05113db2527
jihoon0621 <42331481+jihoon0621@users.noreply.github.com>
2020-09-10 00:46:45 +0900

    #2.16 Local Variables in Pug
    #2.16 Local Variables in Pug

--- app.js
+++ app.js
@@ -3,20 +3,21 @@ import morgan from "morgan";
 import helmet from "helmet";
 import cookieParser from "cookie-parser";
 import bodyParser from "body-parser";
+import { localsMiddleware } from "./middlewares";
+import routes from "./routes";
+import globalRouter from "./routers/globalRouter";
 import userRouter from "./routers/userRouter";
 import videoRouter from "./routers/videoRouter";
-import globalRouter from "./routers/globalRouter";
-import routes from "./routes";
 
 const app = express();
 
+app.use(helmet()); // application 의 안정성을 위한 미들웨어
 app.set("view engine", "pug");
-
 app.use(cookieParser()); // 사용자 인증 시 필요한 미들웨어
 app.use(bodyParser.json()); // 사용자가 웹사이트로 전달하는 정보들을 검사하는 미들웨어
 app.use(bodyParser.urlencoded({ extended: true }));
-app.use(helmet()); // application 의 안정성을 위한 미들웨어
 app.use(morgan("dev")); // application 에서 발생하는 모든 일들을 logging 하는 미들웨어
+app.use(localsMiddleware); // 지역 변수를 전역 변수로 사용할 수 있게 해주는 미들웨어
 
 app.use(routes.home, globalRouter);
 app.use(routes.users, userRouter);
