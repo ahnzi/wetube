a167370632647880426923618afe86f74899d8c2
jihoon0621 <42331481+jihoon0621@users.noreply.github.com>
2020-10-02 22:28:38 +0900

    #2.21 Home Controller
    #2.21 Home Controller

---
+++ db.js
@@ -0,0 +1,50 @@
+export const videos = [
+  {
+    id: 324394,
+    title: "Video awosome",
+    description: "This is something I love",
+    views: 24,
+    videoFile: "https://archive.org/details/BigBuckBunny_124",
+    creator: {
+      id: 121212,
+      name: "Ahnzi",
+      email: "ahnzi@ahnzi.com",
+    },
+  },
+  {
+    id: 235452,
+    title: "Video super",
+    description: "This is something I love",
+    views: 24,
+    videoFile: "https://archive.org/details/BigBuckBunny_124",
+    creator: {
+      id: 121212,
+      name: "Ahnzi",
+      email: "ahnzi@ahnzi.com",
+    },
+  },
+  {
+    id: 124654,
+    title: "Video nice",
+    description: "This is something I love",
+    views: 24,
+    videoFile: "https://archive.org/details/BigBuckBunny_124",
+    creator: {
+      id: 121212,
+      name: "Ahnzi",
+      email: "ahnzi@ahnzi.com",
+    },
+  },
+  {
+    id: 53432,
+    title: "Video perfect",
+    description: "This is something I love",
+    views: 24,
+    videoFile: "https://archive.org/details/BigBuckBunny_124",
+    creator: {
+      id: 121212,
+      name: "Ahnzi",
+      email: "ahnzi@ahnzi.com",
+    },
+  },
+];
