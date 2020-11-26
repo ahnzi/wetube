eda6c08496f0765a1acaaa3f1ed33f9f3d856eb9
안지훈 <42331481+jihoon0621@users.noreply.github.com>
2020-11-26 15:18:04 +0900

    #6.5 MongoStore and Middlewares

--- app.js
+++ app.js
@@ -4,7 +4,9 @@ import helmet from "helmet";
 import cookieParser from "cookie-parser";
 import bodyParser from "body-parser";
 import passport from "passport";
+import mongoose from "mongoose";
 import session from "express-session";
+import MongoStore from "connect-mongo";
 import { localsMiddleware } from "./middlewares";
 import routes from "./routes";
 import globalRouter from "./routers/globalRouter";
@@ -15,6 +17,8 @@ import "./passport";
 
 const app = express();
 
+const CookieStore = MongoStore(session);
+
 app.use(helmet()); // application 의 안정성을 위한 미들웨어
 app.set("view engine", "pug");
 app.use("/uploads", express.static("uploads")); // directory 에서 file 을 보내주는 미들웨어
@@ -23,11 +27,14 @@ app.use(cookieParser()); // 사용자 인증 시 필요한 미들웨어
 app.use(bodyParser.json()); // 사용자가 웹사이트로 전달하는 정보들을 검사하는 미들웨어
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(morgan("dev")); // application 에서 발생하는 모든 일들을 logging 하는 미들웨어
-app.use(session({
-    secret: process.env.COOKIE_SECRET,
-    resave: true,
-    saveUninitialized: false
-}));
+app.use(
+    session({
+        secret: process.env.COOKIE_SECRET,
+        resave: true,
+        saveUninitialized: false,
+        store: new CookieStore({ mongooseConnection: mongoose.connection })
+    })
+);
 app.use(passport.initialize());
 app.use(passport.session());
 
